a37dc99d889f2e11e92a6d84df60860abf8618a3
iagokoch <iagon.koch@gmail.com>
2025-08-18 22:59:12 -0300

    terminando front-end cadastro

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -4,16 +4,24 @@ import "./Logo.css";
 interface LogoProps {
   width?: string;
   className?: string;
+  text?: string;
 }
 
-const Logo: React.FC<LogoProps> = ({ width = "250px", className = "" }) => {
+const Logo: React.FC<LogoProps> = ({
+  width = "170px",
+  className = "",
+  text = "Mais que uma marca, um recomeço.",
+}) => {
   return (
-    <img
-      src="/img/VIREO-LOGO.png"
-      alt="VIREO"
-      className={`logo ${className}`}
-      style={{ width }}
-    />
+    <div className="logo-container">
+      <img
+        src="/img/VIREO-LOGO.png"
+        alt="VIREO"
+        className={`logo ${className}`}
+        style={{ width }}
+      />
+      {text && <span className="logo-text">{text}</span>}
+    </div>
   );
 };
 
--- src/pages/Register/cadastro.tsx
+++ src/pages/Register/cadastro.tsx
@@ -5,10 +5,25 @@ import Logo from "../../components/Logo/Logo";
 const Cadastro: React.FC = () => {
   return (
     <div className="container">
-      <Logo width="250px" />
+      <Logo width="160px" />
 
       <form action="">
         <h1>Cadastre-se</h1>
+        <input type="text" id="name" name="name" placeholder="Nome:" />
+        <input type="email" name="email" id="email" placeholder="Email:" />
+        <input type="tel" name="phone" id="phone" placeholder="Telefone:" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Senha:"
+        />
+        <input
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+          placeholder="Confirme a Senha:"
+        />
       </form>
     </div>
   );
